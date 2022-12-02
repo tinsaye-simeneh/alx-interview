d325e8cb0b2857dd03d65d9983f9fb2bd01bcb41
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2022-12-02 21:43:04 +0300

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,86 +1,44 @@
 #!/usr/bin/python3
-
-"""
-Module 0-validate_utf8
-"""
-
-
-def validUTF8_v1(data):
-    """
-    Determines if a given data set
-    represents a valid UTF-8 encoding
-    """
-    count = 0
-
-    if not data:
-        return False
-
-    for num in data:
-        bin_rep = format(num, '#010b')[-8:]
-
-        if count == 0:
-            for bit in bin_rep:
-                if bit == '0':
-                    break
-                count += 1
-
-            if count == 0:
-                continue
-
-            if count == 1 or count > 4:
-                return False
-        else:
-            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
-                return False
-    return count == 0
+"""Module for validUtf8 method"""
 
 
 def validUTF8(data):
-    count = 0
-
-    if data is None:
-        return False
-
-    for num in data:
-        if count == 0:
-            if num & 128 == 0:
-                count = 0
-            elif num & 224 == 192:
-                count = 1
-            elif num & 240 == 224:
-                count = 2
-            elif num & 248 == 240:
-                count = 3
-            else:
-                return False
-        else:
-            if num & 192 != 128:
-                return False
-    if count == 0:
-        return True
-    return False
-
-
-def validUTF8_v4(data):
+    """Determines if given data represents valid UTF-8 encoding
+    Args:
+        data: list of integers
+    Returns:
+        True if valid UTF-8 encoding, otherwise False
+    """
+    # Number of bytes in the current UTF-8 character
     n_bytes = 0
 
+    # Mask to check if the most significant bit is set or not
     mask1 = 1 << 7
-    mask2 = 1 << 6
 
+    # Mask to check if the second most significant bit is set or not
+    mask2 = 1 << 6
     for num in data:
+
+        # Get the number of set most significant bits in the byte if
+        # this is the starting byte of an UTF-8 character.
         mask = 1 << 7
         if n_bytes == 0:
             while mask & num:
                 n_bytes += 1
                 mask = mask >> 1
 
+            # 1 byte characters
             if n_bytes == 0:
                 continue
 
+            # Invalid scenarios according to the rules of the problem.
             if n_bytes == 1 or n_bytes > 4:
                 return False
         else:
+            # If this byte is a part of an existing UTF-8 character, then we
+            # simply have to look at the two most significant bits and we make
+            # use of the masks we defined before.
             if not (num & mask1 and not (num & mask2)):
                 return False
         n_bytes -= 1
-        return n_bytes == 0
+    return n_bytes == 0
